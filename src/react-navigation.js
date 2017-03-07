788a500857a1007455b39c2967b3f54c6b758c3d
Javier Olaechea <pirata@gmail.com>
2017-03-07 17:42:35 -0500

    export DrawerNavigatorItems

--- src/react-navigation.js
+++ src/react-navigation.js
@@ -26,6 +26,7 @@ module.exports = {
   get CardStack() { return require('./views/CardStack').default; },
   get DrawerView() { return require('./views/Drawer/DrawerView').default; },
   get TabView() { return require('./views/TabView/TabView').default; },
+  get DrawerNavigatorItems() { return require('./views/Drawer/DrawerNavigatorItems').default },
 
   // HOCs
   get withNavigation() { return require('./views/withNavigation').default; },
